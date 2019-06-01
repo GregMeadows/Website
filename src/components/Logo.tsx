bf3a6af2ddb8095a05e31b27969d9a43a05958fc
Greg Meadows <git@gregmeadows.dev>
2019-06-01 18:14:30 +0100

    Add size to logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,26 +1,44 @@
 import React, { FunctionComponent } from 'react';
 import { makeStyles } from '@material-ui/styles';
-import { Typography } from '@material-ui/core';
+import { Typography, Theme } from '@material-ui/core';
 
-const useStyles = makeStyles(() => ({
-    root: {
-        margin: 16
-    },
+interface StyleProps {
+    fontSize: string;
+}
+
+export enum logoSizes {
+    small = '0.5rem',
+    medium = '1rem',
+    large = '1.5rem',
+    massive = '3rem',
+}
+
+const useStyles = makeStyles<Theme, StyleProps>(() => ({
+    root: (props: StyleProps) => ({
+        fontSize: props.fontSize,
+        margin: 16,
+    }),
     name: {
-        fontSize: '12rem',
+        fontSize: '6em',
         fontFamily: 'Alegreya Sans, sans-serif',
+        fontWeight: 'bold'
     },
     title: {
-        fontSize: '4.48rem',
-        margin: '-0.5em 0 0 5.25em',
+        fontSize: '2.06em',
+        margin: '-0.4em 0 0 5.66em',
         color: '#0099ff',
+        fontWeight: 'bold'
     }
 }), {
     classNamePrefix: 'logo',
 });
 
-export const Logo: FunctionComponent = () => {
-    const classes = useStyles();
+export const Logo: FunctionComponent<{
+    size?: logoSizes;
+}> = ({size}) => {
+    let styleProps: StyleProps = {fontSize: `${size || logoSizes.medium}`};
+    const classes = useStyles(styleProps);
+    
     return (
         <div className={classes.root}>
             <Typography variant='h1' className={classes.name}>Greg Meadows</Typography>
