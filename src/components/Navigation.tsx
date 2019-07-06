ecbba2b4e791869a0403cab66df0cf56f1344768
Greg Meadows <git@gregmeadows.dev>
2019-07-06 17:02:49 +0100

    Remove hamburger margin

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -11,13 +11,9 @@ import { Breadcrumb } from './Breadcrumb';
 const useStyles = makeStyles((theme: Theme) => ({
     hamburger: {
         position: 'fixed',
-        marginTop: 10,
-        marginLeft: 10,
         visibility: 'visible',
         [theme.breakpoints.down('xs')]: {
             position: 'absolute',
-            marginTop: 0,
-            marginLeft: 0,
         }
     },
     breadcrumb: {
