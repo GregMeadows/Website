4208aabd6b59c8ba61ddfc300aca0cd9d4d8fb3c
Greg Meadows <git@gregmeadows.dev>
2019-08-26 19:35:32 +0100

    Use axios to submit form

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -2,10 +2,18 @@ import React, { FunctionComponent, FormEvent, useState, ChangeEvent } from 'reac
 import { makeStyles } from '@material-ui/styles';
 import { Typography, TextField, Button, Theme, Box } from '@material-ui/core';
 import SendIcon from '@material-ui/icons/Send';
+import axios from 'axios';
 
-interface formElements {
-    email: string | null;
-    message: string | null;
+interface FormElements {
+    email: string;
+    message: string;
+}
+
+enum FormState {
+    default,
+    validating,
+    sent,
+    error,
 }
 
 const useStyles = makeStyles((theme: Theme) => ({
@@ -28,21 +36,30 @@ const useStyles = makeStyles((theme: Theme) => ({
 
 export const Contact: FunctionComponent = () => {
     const classes = useStyles();
-    const [values, setValues] = useState<formElements>({
-        email: null,
-        message: null,
+    const [values, setValues] = useState<FormElements>({
+        email: '',
+        message: '',
     });
     const [errorText, setErrorText] = useState('');
+    const [formState, setFormState] = useState<FormState>(FormState.default);
 
     function handleSubmit(event: FormEvent<HTMLFormElement>) {
         event.preventDefault();
+        setFormState(FormState.validating);
 
-        if (values.message && values.message.trim() === '') {
-            setErrorText('Please send me something more than 5 letters');
+        if (values.message.trim().length < 10) {
+            setErrorText('Please don\'t spam me with short messages');
+            setFormState(FormState.default);
             return;
         }
 
-        alert('submit');
+        axios.post('/sendMessage.php', values)
+        .then(() => {
+            setFormState(FormState.sent);
+        })
+        .catch(() => {
+            setFormState(FormState.error);
+        });
     }
 
     function handleChange(event: ChangeEvent<HTMLTextAreaElement | HTMLInputElement | HTMLSelectElement>){
@@ -56,65 +73,101 @@ export const Contact: FunctionComponent = () => {
         });
     }
 
+    const successMessage = (
+        <Typography variant='body1'>
+            Thanks for the message! I'll back to you as soon as I can.
+        </Typography>
+    );
+
+    let sideText;
+    if (formState === FormState.default || formState === FormState.validating) {
+        sideText = (
+            <>
+                <Typography variant='body1'>
+                    Use the from to send me a message.
+                </Typography>
+                <Typography variant='body1'>
+                    I'll try to get back to you as quick as possible, so ensure that your email is correct.
+                </Typography>
+            </>
+        );
+    } else if (formState === FormState.error) {
+        sideText = (
+            <>
+                <Typography variant='body1'>
+                    Sorry, there was an issue while sending your message.
+                </Typography>
+                <Typography variant='body1'>
+                    Please feel free to try again.
+                </Typography>
+            </>
+        );
+    }
+
+    const contactForm = (
+        <Box
+            display="flex"
+            flexWrap="wrap"
+        >
+            <form
+                className={classes.form}
+                method="post"
+                onSubmit={handleSubmit}
+            >
+                <TextField
+                    name="email"
+                    label="Email"
+                    variant="outlined"
+                    margin="normal"
+                    type="email"
+                    error={formState === FormState.error}
+                    onChange={e => handleChange(e)}
+                    required
+                />
+                <TextField
+                    name="message"
+                    label="Message"
+                    variant="outlined"
+                    multiline
+                    rows="8"
+                    margin="normal"
+                    error={errorText !== '' || formState === FormState.error}
+                    helperText={errorText}
+                    onChange={e => handleChange(e)}
+                    required
+                />
+                <Box
+                    display="flex"
+                    justifyContent="flex-end"
+                    mt={2}
+                >
+                    <Button
+                        variant="outlined"
+                        color="primary"
+                        type="submit"
+                        disabled={formState !== FormState.default && formState !== FormState.error}
+                    >
+                        <SendIcon className={classes.icon} />
+                        Send Message
+                    </Button>
+                </Box>
+            </form>
+            <Box 
+                display="flex"
+                flexDirection="column"
+                flexGrow={1}
+                flexBasis={0}
+                minWidth={300}
+            >
+                {sideText}
+            </Box>
+        </Box>
+    );
+
     return (
         <section>
             <Typography variant='h1'>Contact Me</Typography>
-            <Box
-                display="flex"
-                flexWrap="wrap"
-            >
-                <form className={classes.form} onSubmit={handleSubmit}>
-                    <TextField
-                        name="email"
-                        label="Email"
-                        variant="outlined"
-                        margin="normal"
-                        type="email"
-                        onChange={e => handleChange(e)}
-                        required
-                    />
-                    <TextField
-                        name="message"
-                        label="Message"
-                        variant="outlined"
-                        multiline
-                        rows="8"
-                        margin="normal"
-                        error={errorText !== ''}
-                        helperText={errorText}
-                        onChange={e => handleChange(e)}
-                        required
-                    />
-                    <Box
-                        display="flex"
-                        justifyContent="flex-end"
-                        mt={2}
-                    >
-                        <Button
-                            variant="outlined"
-                            color="primary"
-                            type="submit"
-                        >
-                            <SendIcon className={classes.icon} />
-                            Send Message
-                        </Button>
-                    </Box>
-                </form>
-                <Box 
-                    display="flex"
-                    flexDirection="column"
-                    flexGrow={1}
-                    flexBasis={0}
-                    minWidth={300}
-                >
-                    <Typography variant='body1'>
-                        Use the from to send me a message.
-                    </Typography>
-                    <Typography variant='body1'>
-                        I'll try to get back to you as quick as possible, so ensure that your email is correct.
-                    </Typography>
-                </Box>
-            </Box>
+            {formState === FormState.sent ? successMessage : contactForm}
         </section>
     );
 };
