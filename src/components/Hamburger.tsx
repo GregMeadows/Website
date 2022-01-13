46c3d174c61d789676dfc6754d195a25043b9d5d
Greg Meadows <git@gregmeadows.dev>
2022-01-13 21:30:55 +0000

    Use framer to animate hamburger

--- src/components/Hamburger.tsx
+++ src/components/Hamburger.tsx
@@ -1,84 +1,66 @@
 import { IconButton } from '@mui/material';
+import { motion } from 'framer-motion';
 import React, { FunctionComponent } from 'react';
 import { makeStyles } from 'tss-react/mui';
 
 const useStyles = makeStyles()((theme) => ({
-  root: {
-    width: 24,
-    height: 20,
-    cursor: 'pointer',
-    zIndex: 1500,
-    position: 'absolute',
-    '&:hover': {
-      '& span': {
-        background: theme.palette.primary.light,
-      },
-    },
-    '& span': {
-      display: 'block',
-      position: 'absolute',
-      height: 4,
-      width: '100%',
-      background: theme.palette.primary.main,
-      borderRadius: 10,
-      opacity: 1,
-      left: 0,
-      transform: 'rotate(0deg)',
-      transition: 'all .15s ease .3s, background .1s linear 0s',
-    },
-    '& span:nth-child(1)': {
-      top: 0,
-    },
-    '& span:nth-child(2), & span:nth-child(3)': {
-      top: 8,
-      transition: 'transform .15s ease 0s, background .1s linear 0s',
-    },
-    '& span:nth-child(4)': {
-      top: 16,
-    },
+  button: {
+    padding: '0.6rem',
   },
-  active: {
-    '& span:nth-child(1), & span:nth-child(4)': {
-      top: 8,
-      opacity: 0,
-      transition: 'all .3s ease 0s',
-    },
-    '& span:nth-child(2), & span:nth-child(3)': {
-      background: theme.palette.secondary.main,
-      transition: 'transform .15s ease .3s, background .1s linear .15s',
-    },
-    '& span:nth-child(2)': {
-      transform: 'rotate(45deg)',
-    },
-    '& span:nth-child(3)': {
-      transform: 'rotate(-45deg)',
-    },
+  icon: {
+    height: '2rem',
+    width: '2rem',
+  },
+  path: {
+    strokeWidth: 3,
+    stroke: theme.palette.primary.main,
+    strokeLinecap: 'round',
   },
 }));
 
 interface HamburgerProps {
   onClick: () => void;
-  state: boolean;
   className?: string;
 }
 
 const Hamburger: FunctionComponent<HamburgerProps> = function Hamburger({
   onClick,
-  state,
   className,
 }) {
-  const { classes } = useStyles();
+  const { classes, cx } = useStyles();
 
   return (
     <IconButton
-      className={`${classes.root} ${state ? classes.active : ''} ${className}`}
+      className={cx(className, classes.button)}
       aria-label="menu"
       onClick={onClick}
+      color="primary"
     >
-      <span />
-      <span />
-      <span />
-      <span />
+      <svg viewBox="0 0 22 18" className={classes.icon}>
+        <motion.path
+          className={classes.path}
+          variants={{
+            closed: { d: 'M 2 2.5 L 20 2.5' },
+            open: { d: 'M 3 16.5 L 17 2.5' },
+          }}
+        />
+        <motion.path
+          className={classes.path}
+          d="M 2 9.423 L 20 9.423"
+          variants={{
+            closed: { opacity: 1 },
+            open: { opacity: 0 },
+          }}
+          transition={{ duration: 0.1 }}
+        />
+        <motion.path
+          className={classes.path}
+          variants={{
+            closed: { d: 'M 2 16.346 L 20 16.346' },
+            open: { d: 'M 3 2.5 L 17 16.346' },
+          }}
+        />
+      </svg>
     </IconButton>
   );
 };
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,56 +1,15 @@
-import {
-  AppBar,
-  Grid,
-  SwipeableDrawer,
-  Toolbar,
-  useMediaQuery,
-  useTheme,
-} from '@mui/material';
-import React, { FunctionComponent, useEffect, useState } from 'react';
+import { Grid, useMediaQuery, useTheme } from '@mui/material';
+import { motion, useCycle } from 'framer-motion';
+import React, { FunctionComponent, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { makeStyles } from 'tss-react/mui';
-import Breadcrumb from './Breadcrumb';
 import Hamburger from './Hamburger';
-import Logo, { logoSizes } from './Logo';
+import Logo from './Logo';
 import NavigationItems from './NavigationItems';
 
 const useStyles = makeStyles()((theme) => ({
   hamburger: {
     position: 'fixed',
-    visibility: 'visible',
-    [theme.breakpoints.down('sm')]: {
-      position: 'absolute',
-    },
-  },
-  breadcrumb: {
-    zIndex: 1499,
-    transition: '.3s ease 0s, opacity .1s ease 0s, visibility 0s linear .5s',
-    opacity: 0,
-    visibility: 'hidden',
-  },
-  activeBread: {
-    transition: '.5s ease .3s',
-    marginLeft: 30,
-    opacity: 1,
-    visibility: 'visible',
-  },
-  appBar: {
-    background: theme.palette.background.default,
-    zIndex: theme.zIndex.drawer + 101,
-    [theme.breakpoints.up('sm')]: {
-      visibility: 'hidden',
-    },
-  },
-  appBarLogo: {
-    display: 'flex',
-    flexGrow: 1,
-    justifyContent: 'center',
-    transition: 'opacity .1s ease .3s, visibility 0s linear 0s',
-  },
-  appBarLogoHidden: {
-    visibility: 'hidden',
-    opacity: 0,
-    transition: 'opacity .1s ease 0s, visibility 0s linear .3s',
   },
   drawerLayout: {
     marginTop: '2%',
@@ -59,126 +18,59 @@ const useStyles = makeStyles()((theme) => ({
       width: '18rem',
     },
   },
-  toolbar: {
-    height: theme.spacing(7),
-  },
-  item: {
-    margin: '4% 6%',
-    '& h2': {
-      fontSize: '3rem',
-    },
-    [theme.breakpoints.down(800)]: {
-      margin: '4%',
-      '& h2': {
-        fontSize: '2.5rem',
-      },
-    },
-    [theme.breakpoints.down('sm')]: {
-      margin: 0,
-      '& h2': {
-        fontSize: '2rem',
-      },
-      '&:hover': {
-        background: theme.palette.background.highlight,
-      },
-    },
-  },
-  link: {
-    color: theme.palette.text.primary,
-    textAlign: 'center',
-    display: 'block',
-    '&:hover': {
-      color: theme.palette.secondary.main,
-    },
-    [theme.breakpoints.down('sm')]: {
-      padding: '4% 0',
-      paddingLeft: 50,
-      textAlign: 'left',
-      borderBottom: `1px solid ${theme.palette.background.highlight}`,
-      '&:hover': {
-        color: theme.palette.text.primary,
-      },
-    },
-  },
-  mobileDisplayNone: {
-    [theme.breakpoints.down('sm')]: {
-      display: 'none',
-    },
-  },
-  mobileDisplayBlock: {
-    [theme.breakpoints.down('sm')]: {
-      display: 'block',
-    },
-  },
-  desktopDisplayNone: {
-    [theme.breakpoints.up(800)]: {
-      display: 'none',
-    },
-  },
 }));
 
 const Navigation: FunctionComponent = function Navigation() {
   const { classes } = useStyles();
   const theme = useTheme();
-  const widthMobile = useMediaQuery(theme.breakpoints.down('md'));
-  const drawerAnchor = widthMobile ? 'left' : 'top';
+  const isMobile = useMediaQuery(theme.breakpoints.down('md'));
 
-  // Drawer State
-  const [showNav, setShowNav] = useState(false);
-  const toggleNav = (state?: boolean) => () => {
-    setShowNav(state || !showNav);
-  };
+  const [isOpen, toggleOpen] = useCycle(false, true);
 
   // Close menu on scene change
   const { pathname, search } = useLocation();
   useEffect(() => {
-    setShowNav(false);
+    toggleOpen(0);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [pathname, search]);
 
+  const sidebar = {
+    open: (height = 1000) => ({
+      clipPath: `circle(${height * 2 + 200}px at 40px 40px)`,
+      transition: {
+        type: 'spring',
+        stiffness: 20,
+        restDelta: 2,
+      },
+    }),
+    closed: {
+      clipPath: 'circle(30px at 40px 40px)',
+      transition: {
+        delay: 0.5,
+        type: 'spring',
+        stiffness: 400,
+        damping: 40,
+      },
+    },
+  };
+
   return (
-    <nav>
-      <AppBar position="fixed" className={classes.appBar}>
-        <Toolbar>
-          <Hamburger
-            onClick={toggleNav()}
-            state={showNav}
-            className={classes.hamburger}
-          />
-          <Breadcrumb
-            className={`${classes.breadcrumb} ${
-              showNav ? classes.activeBread : ''
-            } ${classes.hamburger}`}
-          />
-          <div
-            className={`${classes.appBarLogo} ${
-              showNav ? classes.appBarLogoHidden : ''
-            }`}
-          >
-            <Logo size={logoSizes.xs} />
-          </div>
-        </Toolbar>
-      </AppBar>
-      <SwipeableDrawer
-        anchor={drawerAnchor}
-        open={showNav}
-        onClose={toggleNav(false)}
-        onOpen={toggleNav(true)}
+    <motion.nav initial={false} animate={isOpen ? 'open' : 'closed'}>
+      <motion.div className="background" variants={sidebar} />
+      <Hamburger onClick={() => toggleOpen()} className={classes.hamburger} />
+      <Grid
+        container
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        className={classes.drawerLayout}
       >
-        <div className={`${classes.toolbar} ${classes.desktopDisplayNone}`} />
-        <Grid
-          container
-          direction="column"
-          alignItems="center"
-          justifyContent="center"
-          className={`${classes.drawerLayout} ${classes.mobileDisplayBlock}`}
-        >
-          <Grid item className={classes.mobileDisplayNone}>
-            <Logo />
-          </Grid>
-          <NavigationItems showIcons />
+        <Grid item>
+          <Logo />
         </Grid>
-      </SwipeableDrawer>
-    </nav>
+        <NavigationItems showIcons />
+      </Grid>
+    </motion.nav>
   );
 };
 
